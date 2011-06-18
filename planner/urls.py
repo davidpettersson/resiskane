c427f8caf681b9c5579d74248863454ae5bc31c6
David Pettersson <1101727+davidpettersson@users.noreply.github.com>
2011-06-19 00:33:02 +0200

    Snapshot of current functionality.

--- planner/urls.py
+++ planner/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import *
 
-from views import start, search, css, ajax_stations
+from views import start, search, css, robots, ajax_stations
 
 urlpatterns = patterns('',
     # Example:
     (r'^$', start),
     (r'^search', search),
     (r'^css', css),
+    (r'^robots.txt$', robots),
     (r'^ajax/stations', ajax_stations),
 )
