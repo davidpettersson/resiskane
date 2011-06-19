c7e2f69bd1d7640c590de1853dd9b4f53b1b40af
David Pettersson <1101727+davidpettersson@users.noreply.github.com>
2011-06-19 20:35:11 +0200

    Revert static file changes -- static files are served via web server aliases.

--- settings.py
+++ settings.py
@@ -56,14 +56,11 @@ TEMPLATE_DIRS = (
 
 )
 
-STATIC_URL= '/static/'
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'resiskane.planner',
 )
