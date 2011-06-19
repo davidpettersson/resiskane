cd0838af13c9bd8251f1279ad5e10405f7362c17
Ian Kumlien <pomac@vapor.com>
2011-06-19 18:42:01 +0200

    Make it able to serve static files locally.
    One problem is that the static files are looked for in planner/static, which
    is not it's actual location.
    
    We should discuss what to do.

--- settings.py
+++ settings.py
@@ -56,11 +56,14 @@ TEMPLATE_DIRS = (
 
 )
 
+STATIC_URL= '/static/'
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'resiskane.planner',
 )
