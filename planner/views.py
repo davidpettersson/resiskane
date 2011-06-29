fcfab5ee21aa9f491cb9e3fd49c4dd4e881e8d7d
David Pettersson <1101727+davidpettersson@users.noreply.github.com>
2011-06-29 02:45:23 +0200

    Two more alternatives.

--- planner/views.py
+++ planner/views.py
@@ -156,8 +156,8 @@ def search(req):
     info = { 
         'fr': r_fr[0].getName(),
         'to': r_to[0].getName(),
-        'fr_alts': map(lambda x: x.getName(), r_fr[1:4]),
-        'to_alts': map(lambda x: x.getName(), r_to[1:4]),
+        'fr_alts': map(lambda x: x.getName(), r_fr[1:6]),
+        'to_alts': map(lambda x: x.getName(), r_to[1:6]),
         'journeys': journeys,
         }
     return render_to_response('search.html', info)
