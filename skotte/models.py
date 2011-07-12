bc85f99b4cceadf3cbd790957670d24e6c68c0e8
davidpettersson <davidpettersson@localhost>
2011-07-12 02:29:57 +0200

    Added indexes to speed up searches.

--- skotte/models.py
+++ skotte/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Station(models.Model):
     identifier = models.IntegerField(unique=True)
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, db_index=True)
     x = models.IntegerField()
     y = models.IntegerField()
-    metaphone = models.CharField(max_length=128)
+    metaphone = models.CharField(max_length=128, db_index=True)
     refreshed = models.DateTimeField(auto_now=True, auto_now_add=True)
